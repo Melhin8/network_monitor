c3454affbf16e5a8fcb7e8dbfce19e984dfc9a21
melhin8 <melhin8@gmail.com>
2022-06-29 12:53:27 +0300

    add docksrings

--- network_monitoring.py
+++ network_monitoring.py
@@ -4,11 +4,27 @@ import subprocess
 from time import sleep
 
 
-def bash_call(param: str) -> None:
+def bash_call(param: str) -> int:
+    """run nmcli in bash
+
+    Args:
+        param (str): parameters for nmcli
+
+    Returns:
+        subprocess.call (int): subprocess code
+    """
     return subprocess.call(f'nmcli {param}', shell=True)
 
 
 def bash_output(param: str) -> str:
+    """run command in bash and return its output
+
+    Args:
+        param (str): command with parametrs
+
+    Returns:
+        str: commands output
+    """
     return subprocess.check_output(param, shell=True).decode('utf-8')
 
 
@@ -27,6 +43,11 @@ def reboot_wifi(hotspot: str) -> None:
 
 
 def select_device() -> str:
+    """scan and select network adapter
+
+    Returns:
+        str: selected adapter
+    """
     dirty_arp = bash_output('nmcli d | grep wifi')
     list_dirty_arp = [i.split() for i in dirty_arp.split('\n')]
     device = [i[0] for i in list_dirty_arp if 'wifi' in i]
@@ -34,10 +55,28 @@ def select_device() -> str:
 
 
 def select_hotspot(device=select_device()) -> str:
+    """run `nmcli c` and filtred by wifi and selected network adapter
+
+    Args:
+        device (str, optional): selected network adapter. Defaults to select_device().
+
+    Returns:
+        str: name wifi conection
+    """
     return bash_output(f'nmcli c | awk "/wifi/ && /{device}/"').split()[0]
 
 
-def ping(host: str, params: str, grep: str) -> (str):
+def ping(host: str, params: str, grep: str) -> str:
+    """run ping with some parameters and grep
+
+    Args:
+        host (str): host who ping
+        params (str): params to ping
+        grep (str): what exactly grep with word `grep`
+
+    Returns:
+        str: ping output
+    """
     try:
         pong = bash_output(f'ping {host} {params} | {grep}')
     except Exception:
@@ -47,6 +86,15 @@ def ping(host: str, params: str, grep: str) -> (str):
 
 
 def select_ip(device=select_device()) -> str:
+    """scan local network and select ip with min avg ping.
+    If wifi adapter is down - up it
+
+    Args:
+        device (str, optional): selected network adapter. Defaults to select_device().
+
+    Returns:
+        str: selected ip
+    """
     try:
         arp = bash_output(f'arp -ni {device} | grep ether')
     except Exception:
@@ -72,7 +120,15 @@ def select_ip(device=select_device()) -> str:
     return selected_ip
 
 
-def local_ping(selected_ip: str) -> str:
+def local_ping(selected_ip: str) -> tuple(str, str):
+    """ping ip and change it if ping is None
+
+    Args:
+        selected_ip (str): ip with min avg ping
+
+    Returns:
+        tuple (str, str): (output ping, ip what ping)
+    """
     dirty_avg = ping(selected_ip, '-c 5', 'grep /')
     if dirty_avg is None:
         logging.error('Local ping is None')
@@ -82,13 +138,29 @@ def local_ping(selected_ip: str) -> str:
     return dirty_avg, selected_ip
 
 
-def local_avg(selected_ip: str) -> float:
+def local_avg(selected_ip: str) -> tuple(float, str):
+    """checks `local_ping` and filters the avg ping
+
+    Args:
+        selected_ip (str): ip with min avg ping
+
+    Returns:
+        tuple(float, str): (avg ping, ip what ping)
+    """
     dirty_avg, selected_ip = local_ping(selected_ip)
     avg_local = float(dirty_avg.split('=')[1].split('/')[1])
     return avg_local, selected_ip
 
 
 def host_ping(host: str) -> str:
+    """ping host
+
+    Args:
+        host (str): 8.8.8.8
+
+    Returns:
+        str: host ping output
+    """
     host_stats = ping(host, '-c 3', 'grep %')
     if host_stats is None:
         logging.error('Host ping is None')
@@ -98,12 +170,27 @@ def host_ping(host: str) -> str:
 
 
 def host_received(host: str) -> int:
+    """cheks `host_ping` and filtered number of received packages
+
+    Args:
+        host (str): 8.8.8.8
+
+    Returns:
+        int: number of received packages
+    """
     host_stats = host_ping(host)
     received = int(''.join(filter(str.isdigit, host_stats.split(',')[1])))
     return received
 
 
 def monitoring(selected_ip: str, host: str, hotspot: str) -> None:
+    """main function. collect data and restart Wi-Fi or network when needed
+
+    Args:
+        selected_ip (str): ip with min avg ping
+        host (str): 8.8.8.8
+        hotspot (str): name wifi conection
+    """
     while True:
         avg_local, selected_ip = local_avg(selected_ip)
         received = host_received(host)
@@ -133,6 +220,5 @@ if __name__ == "__main__":
     host = '8.8.8.8'
     selected_ip = select_ip()
     hotspot = select_hotspot()
-    device = select_device()
     monitoring(selected_ip, host, hotspot)
     logging.info('Stoped session')
